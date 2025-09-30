1bf8de494cc99fdaa0e569deaef8e9afbd5c6e6c
Elisa Maurín <230084@fppiramide.com>
2025-09-30 18:37:38 +0200

    Seguimos progresando

--- src/main/java/com/cpifppiramide/demo/DemoController/Controller.java
+++ src/main/java/com/cpifppiramide/demo/DemoController/Controller.java
@@ -1,24 +1,47 @@
 package com.cpifppiramide.demo.DemoController;
 
+import com.cpifppiramide.demo.clases.Usuario;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+    private List<Usuario> usuarios =  new ArrayList<>();
 
     @RequestMapping("/index")
     String inicio(){
         return "index";
     }
 
-    @RequestMapping("/registarse")
+    @GetMapping ("/registrarse")
     String registro(){
-        return "registro";
+        return "registrarse";
     }
-
+    @PostMapping
+    String registrar(Model model, Usuario usuario){
+        boolean existe = usuario.getNombreUsuario().equals(usuario.getNombreUsuario());
+        if(existe){
+            model.addAttribute("mensaje","Este usuario ya existe");
+            return "registrarse";
+        }else{
+            usuarios.add(usuario);
+        }
+        return "inicioSesion";
+    }
     @RequestMapping("/inicioSesion")
     String inicioSesion(){
         return "inicioSesion";
     }
+
+
+
+
 
 
 }
